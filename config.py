a0727f82771aba7d53c4a5ae0f1fb7da4c578608
Manuel Chinchi <manuel.chinchi93@gmail.com>
2025-02-10 16:41:50 -0300

    FIXME: add config.py missing file (last commit)

---
+++ config.py
@@ -0,0 +1,27 @@
+APP_TITLE = "Adminstrador de procesos"
+APP_ICON = "taskmgr.exe_14_107.ico"
+WINDOW_SIZE = "800x500"
+
+COLUMN_ID = "Id"
+COLUMN_PROCESS_NAME = "ProcessName"
+COLUMN_STATUS = "Status"
+# COLUMN_DESCRIPTION = "Descripción"
+
+COLUMN_HEADERS = {
+    COLUMN_ID : "ID",
+    COLUMN_PROCESS_NAME : "Nombre del proceso",
+    COLUMN_STATUS : "Estado"
+}
+
+BUTTON_SEARCH = 100
+BUTTON_UPDATE = 101
+LABEL_TOTAL = 102
+# BUTTON_SEARCH = ""
+BOTTOM_FRAME = {
+    BUTTON_SEARCH : "Buscar",
+    BUTTON_UPDATE : "Actualizar",
+    LABEL_TOTAL : "Total"
+}
+
+SORT_ASC_ICON = "▲"
+SORT_DESC_ICON = "▼"
