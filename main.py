0bb7e3ae473453074daec6b88c2aa0669ce9a894
Manuel Chinchi <manuel.chinchi93@gmail.com>
2025-02-12 13:57:54 -0300

    minnor refactor code

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ center_window(root)
 root.deiconify()
 
 # Resto del código (frame_tabla, tabla, scrollbar, etc.)
-frame_process_table = tk.Frame(root)
-frame_process_table.pack(expand=True, fill="both")
+frame_main = tk.Frame(root)
+frame_main.pack(expand=True, fill="both")
 
 style = ttk.Style(root)
 style.configure("Custom.Treeview",
@@ -56,77 +56,77 @@ style.configure("Custom.Treeview",
 
 # TODO Estilo original cabeceras
 # style.configure("Treeview.Heading", background="#B0E0E6", foreground="#0078D7", font=("TkDefaultFont", 10, "bold"))
-process_table = ttk.Treeview(frame_process_table, 
+tree_processes = ttk.Treeview(frame_main, 
                     columns=(config.COLUMN_ID, config.COLUMN_PROCESS_NAME, config.COLUMN_STATUS, config.COLUMN_LOCATION),
                     show="headings", 
                     style="Custom.Treeview")
-process_table.heading(config.COLUMN_ID, text=config.COLUMN_HEADERS[config.COLUMN_ID], anchor='w', command=lambda: sort_column(config.COLUMN_ID))
-process_table.heading(config.COLUMN_PROCESS_NAME, text=config.COLUMN_HEADERS[config.COLUMN_PROCESS_NAME],anchor='w', command=lambda: sort_column(config.COLUMN_PROCESS_NAME))
-process_table.heading(config.COLUMN_STATUS, text=config.COLUMN_HEADERS[config.COLUMN_STATUS], anchor='w', command=lambda: sort_column(config.COLUMN_STATUS))
-process_table.heading(config.COLUMN_LOCATION, text=config.COLUMN_HEADERS[config.COLUMN_LOCATION], anchor='w', command=lambda: sort_column(config.COLUMN_LOCATION))
+tree_processes.heading(config.COLUMN_ID, text=config.COLUMN_HEADERS[config.COLUMN_ID], anchor='w', command=lambda: sort_column(config.COLUMN_ID))
+tree_processes.heading(config.COLUMN_PROCESS_NAME, text=config.COLUMN_HEADERS[config.COLUMN_PROCESS_NAME],anchor='w', command=lambda: sort_column(config.COLUMN_PROCESS_NAME))
+tree_processes.heading(config.COLUMN_STATUS, text=config.COLUMN_HEADERS[config.COLUMN_STATUS], anchor='w', command=lambda: sort_column(config.COLUMN_STATUS))
+tree_processes.heading(config.COLUMN_LOCATION, text=config.COLUMN_HEADERS[config.COLUMN_LOCATION], anchor='w', command=lambda: sort_column(config.COLUMN_LOCATION))
 
-process_table.column(config.COLUMN_ID, width=5, anchor="w", minwidth=75, stretch=True)
-process_table.column(config.COLUMN_PROCESS_NAME, width=20, anchor="w", minwidth=120, stretch=True)
-process_table.column(config.COLUMN_STATUS, width=10, anchor="w", minwidth=100, stretch=True)
-process_table.column(config.COLUMN_LOCATION, width=150, anchor="w", minwidth=150, stretch=True)
+tree_processes.column(config.COLUMN_ID, width=5, anchor="w", minwidth=75, stretch=True)
+tree_processes.column(config.COLUMN_PROCESS_NAME, width=20, anchor="w", minwidth=120, stretch=True)
+tree_processes.column(config.COLUMN_STATUS, width=10, anchor="w", minwidth=100, stretch=True)
+tree_processes.column(config.COLUMN_LOCATION, width=150, anchor="w", minwidth=150, stretch=True)
 
-scrollbar = tk.Scrollbar(frame_process_table, orient="vertical", command=process_table.yview)
-scrollbar.pack(side="right", fill="y")
+scrollbar_tree = tk.Scrollbar(frame_main, orient="vertical", command=tree_processes.yview)
+scrollbar_tree.pack(side="right", fill="y")
 
-process_table.config(yscrollcommand=scrollbar.set)
-process_table.pack(expand=True, fill="both")
+tree_processes.config(yscrollcommand=scrollbar_tree.set)
+tree_processes.pack(expand=True, fill="both")
 
 orden_ascendente = {config.COLUMN_ID: True, config.COLUMN_PROCESS_NAME: True, config.COLUMN_STATUS: True, config.COLUMN_LOCATION: True}
 all_processes = []
 
 def update_table():
     global all_processes
-    for row in process_table.get_children():
-        process_table.delete(row)
+    for process in tree_processes.get_children():
+        tree_processes.delete(process)
 
     all_processes = []
-    for p in psutil.process_iter(attrs=['pid', 'name', 'status']):
+    for process in psutil.process_iter(attrs=['pid', 'name', 'status']):
         try:
-            pid = p.info['pid']
-            name = p.info['name']
-            status = p.info['status']
+            pid = process.info['pid']
+            name = process.info['name']
+            status = process.info['status']
             location = psutil.Process(pid).exe()
             all_processes.append((pid, name, status, location))
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             continue
 
     for pid, name, status, location in all_processes:
-        process_table.insert("", tk.END, values=(pid, name, status, location))
+        tree_processes.insert("", tk.END, values=(pid, name, status, location))
 
     lbl_total.config(text=f"{config.BOTTOM_FRAME[config.LABEL_TOTAL]}: {len(all_processes)}")
 
-    process_table.heading(column=config.COLUMN_ID, text=f"{config.COLUMN_HEADERS[config.COLUMN_ID]} {config.SORT_DESC_ICON}")
-    process_table.heading(column=config.COLUMN_PROCESS_NAME, text=config.COLUMN_HEADERS[config.COLUMN_PROCESS_NAME])
-    process_table.heading(column=config.COLUMN_STATUS, text=config.COLUMN_HEADERS[config.COLUMN_STATUS])
-    process_table.heading(column=config.COLUMN_LOCATION, text=config.COLUMN_HEADERS[config.COLUMN_LOCATION])
+    tree_processes.heading(column=config.COLUMN_ID, text=f"{config.COLUMN_HEADERS[config.COLUMN_ID]} {config.SORT_DESC_ICON}")
+    tree_processes.heading(column=config.COLUMN_PROCESS_NAME, text=config.COLUMN_HEADERS[config.COLUMN_PROCESS_NAME])
+    tree_processes.heading(column=config.COLUMN_STATUS, text=config.COLUMN_HEADERS[config.COLUMN_STATUS])
+    tree_processes.heading(column=config.COLUMN_LOCATION, text=config.COLUMN_HEADERS[config.COLUMN_LOCATION])
 
 def sort_column(column):
     global orden_ascendente
-    datos = [(process_table.item(row)["values"][0], process_table.item(row)["values"][1], process_table.item(row)["values"][2], process_table.item(row)["values"][3]) for row in process_table.get_children()]
+    data = [(tree_processes.item(row)["values"][0], tree_processes.item(row)["values"][1], tree_processes.item(row)["values"][2], tree_processes.item(row)["values"][3]) for row in tree_processes.get_children()]
 
     if column == config.COLUMN_ID:
-        datos.sort(key=lambda x: int(x[0]), reverse=not orden_ascendente[config.COLUMN_ID])
+        data.sort(key=lambda x: int(x[0]), reverse=not orden_ascendente[config.COLUMN_ID])
         orden_ascendente[config.COLUMN_ID] = not orden_ascendente[config.COLUMN_ID]
     elif column == config.COLUMN_PROCESS_NAME:
-        datos.sort(key=lambda x: x[1].lower(), reverse=not orden_ascendente[config.COLUMN_PROCESS_NAME])
+        data.sort(key=lambda x: x[1].lower(), reverse=not orden_ascendente[config.COLUMN_PROCESS_NAME])
         orden_ascendente[config.COLUMN_PROCESS_NAME] = not orden_ascendente[config.COLUMN_PROCESS_NAME]
     elif column == config.COLUMN_STATUS:
-        datos.sort(key=lambda x: x[2].lower(), reverse=not orden_ascendente[config.COLUMN_STATUS])
+        data.sort(key=lambda x: x[2].lower(), reverse=not orden_ascendente[config.COLUMN_STATUS])
         orden_ascendente[config.COLUMN_STATUS] = not orden_ascendente[config.COLUMN_STATUS]
     elif column == config.COLUMN_LOCATION:
-        datos.sort(key=lambda x: x[3].lower(), reverse=not orden_ascendente[config.COLUMN_LOCATION])
+        data.sort(key=lambda x: x[3].lower(), reverse=not orden_ascendente[config.COLUMN_LOCATION])
         orden_ascendente[config.COLUMN_LOCATION] = not orden_ascendente[config.COLUMN_LOCATION]
 
-    for row in process_table.get_children():
-        process_table.delete(row)
+    for row in tree_processes.get_children():
+        tree_processes.delete(row)
 
-    for pid, name, status, location in datos:
-        process_table.insert("", "end", values=(pid, name, status, location))
+    for pid, name, status, location in data:
+        tree_processes.insert("", "end", values=(pid, name, status, location))
 
     process_list = [config.COLUMN_ID, config.COLUMN_PROCESS_NAME, config.COLUMN_STATUS, config.COLUMN_LOCATION]
     for col in process_list:
@@ -135,13 +135,13 @@ def sort_column(column):
         else:
             symbol = ""
         text = config.COLUMN_HEADERS[config.COLUMN_ID] if col == config.COLUMN_ID else config.COLUMN_HEADERS[config.COLUMN_PROCESS_NAME] if col == config.COLUMN_PROCESS_NAME else config.COLUMN_HEADERS[config.COLUMN_STATUS] if col == config.COLUMN_STATUS else config.COLUMN_HEADERS[config.COLUMN_LOCATION]
-        process_table.heading(col, text=f"{text} {symbol}")
+        tree_processes.heading(col, text=f"{text} {symbol}")
 
 def filter_process():
     text = entry_search.get().strip().lower()
 
-    for row in process_table.get_children():
-        process_table.delete(row)
+    for process in tree_processes.get_children():
+        tree_processes.delete(process)
 
     if text == "":
         filtered_data = all_processes
@@ -149,26 +149,26 @@ def filter_process():
         filtered_data = [(pid, name, status, location) for pid, name, status, location in all_processes if text in name.lower()]
 
     for pid, name, status, location in filtered_data:
-        process_table.insert("", "end", values=(pid, name, status, location))
+        tree_processes.insert("", "end", values=(pid, name, status, location))
 
     lbl_total.config(text=f"{config.BOTTOM_FRAME[config.LABEL_TOTAL]}: {len(filtered_data)}")
 
 def auto_adjust_columns():
     """Ajusta automáticamente el ancho de las columnas al mínimo necesario, respetando minwidth"""
-    if auto_adjust_var.get():  # Solo ajustar si el checkbox está marcado
-        process_table.update_idletasks()  # Actualizar la geometría de la tabla
-        for col in process_table["columns"]:
+    if auto_adjust_cols.get():  # Solo ajustar si el checkbox está marcado
+        tree_processes.update_idletasks()  # Actualizar la geometría de la tabla
+        for col in tree_processes["columns"]:
             # Obtener el valor de minwidth de la columna
-            min_width = int(process_table.column(col, option="minwidth"))
+            min_width = int(tree_processes.column(col, option="minwidth"))
 
             # Obtener solo las filas visibles (las que están en la tabla después del filtro)
-            visible_rows = process_table.get_children()
+            visible_rows = tree_processes.get_children()
 
             # Calcular el ancho máximo de la columna basado en el contenido de las celdas visibles
             # #FIXME Error si hay 0 resultados 
             # Si no hay resultados al filtrar se produce un error en esta linea
             max_width = max(
-                font.Font().measure(str(process_table.set(row, col)))  # Medir el ancho del texto
+                font.Font().measure(str(tree_processes.set(row, col)))  # Medir el ancho del texto
                 for row in visible_rows  # Iterar solo sobre las filas visibles
             )
 
@@ -176,9 +176,9 @@ def auto_adjust_columns():
             adjusted_width = max(max_width + 10, min_width)
 
             # Ajustar el ancho de la columna
-            process_table.column(col, width=adjusted_width)
+            tree_processes.column(col, width=adjusted_width)
 
-auto_adjust_var = tk.BooleanVar(value=True)  # Por defecto, el ajuste automático está habilitado
+auto_adjust_cols = tk.BooleanVar(value=True)  # Por defecto, el ajuste automático está habilitado
 enable_dark_theme = tk.BooleanVar(value=False) # Tema oscuro
 
 # FIXME Problema al cliquear controles (tema oscuro)
@@ -194,7 +194,7 @@ def apply_theme(theme):
     hacky.set_bg_color_title_bar(root, color=theme["name"])
     # Configurar colores para widgets de tkinter
     root.config(bg=theme["bg"])
-    frame_process_table.config(bg=theme["bg"])
+    frame_main.config(bg=theme["bg"])
     frame_controls.config(bg=theme["bg"])
     lbl_total.config(bg=theme["bg"], fg=theme["fg"])
     entry_search.config(bg=theme["button_bg"], fg=theme["fg"], insertbackground=theme["fg"])
@@ -267,7 +267,7 @@ def open_settings_popup():
         frame_checks = tk.Frame(popup)
         frame_checks.pack(padx=10, pady=10, anchor="w")
 
-        chk_auto_adjust_cols = tk.Checkbutton(frame_checks, text=config.SETTINGS_OPTIONS[config.CHECKBOX_ADJUST_AUTOMATIC_COLS], variable=auto_adjust_var)
+        chk_auto_adjust_cols = tk.Checkbutton(frame_checks, text=config.SETTINGS_OPTIONS[config.CHECKBOX_ADJUST_AUTOMATIC_COLS], variable=auto_adjust_cols)
         chk_auto_adjust_cols.pack(anchor="w")
 
         chk_enable_dark_theme = tk.Checkbutton(frame_checks, text=config.SETTINGS_OPTIONS[config.CHECKBOX_DARK_THEME], variable=enable_dark_theme, command=toggle_theme)
